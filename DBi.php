bcd5cdd896e40a0f96349edae5ad2d49ae8395d2
Wolfgang Drescher <drescher.wolfgang+github@gmail.com>
2014-07-26 15:26:05 +0200

    Add classes

---
+++ DBi.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * DBi.php
+ * Wolfgang Drescher - wolfgangdrescher.ch
+ * This class allows you to connect to a MySQL database with the PHP MySQLi class
+ * and handles multiple connections.
+ */
+
+require_once 'Query.php';
+
+class DBi {
+	
+	public static $autoSelect = true;
+	private static $databases = array();
+	private static $currentConnection = null;
+	
+	public static function connect($host, $user = null, $password = null, $dbname = null, $port = null) {
+		try {
+			$connection = @new MySQLi($host, $user, $password, $dbname, $port);
+			if($connection->connect_errno) {
+				throw new Exception($connection->connect_error, $connection->connect_errno);
+			} else {
+				return $connection;
+			}
+		} catch (Exception $e) {
+			ob_start();
+			echo '<div class="alert alert-danger">';
+			echo "<b>Unable to connect to the MySQL database '$dbname'</b> (#" . $e->getCode() . ").<br/>";
+			echo '<i>' . $e->getMessage() . '</i>';
+			echo '</div>';
+			die(ob_get_clean());
+		}
+	}
+	
+	// Adds a connection to the DBi::$databases array
+	public static function add($connection, $key = null) {
+		if(self::isConnection($connection)) {
+			if($key === null) {
+				self::$databases[] = $connection;
+			} else {
+				self::$databases[$key] = $connection;
+			}
+			if(self::$autoSelect === true) {
+				self::set($connection);
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	// Sets a connection to the currently used connection
+	public static function set($connection) {
+		if(self::isConnection(self::get($connection))) {
+			self::$currentConnection = self::get($connection);
+		} elseif(self::isConnection($connection)) {
+			self::$currentConnection = $connection;
+		}
+	}
+	
+	// Returns a connection out of the DBi::$databases array or DBi::$currentConnection
+	public static function get($key = null) {
+		if($key === null) {
+			if(self::$currentConnection !== null) {
+				return self::$currentConnection;
+			} elseif(count(self::$databases) === 1) {
+				return array_shift(array_values(self::$databases));
+			}
+		} else {
+			if(array_key_exists($key, self::$databases)) {
+				return self::$databases[$key];
+			} elseif(is_int($key) AND array_key_exists($key, $arrayValues = array_values(self::$databases))) {
+				return $arrayValues[$key];
+			}
+		}
+		return null;
+	}
+	
+	// Checks if a connection is a valid MySQL connection
+	public static function isConnection($connection) {
+		return (
+			$connection !== null AND
+			gettype($connection) == 'object' AND
+			strtolower(get_class($connection)) == 'mysqli' AND
+			$connection->connect_errno === 0
+		) ? true : false;
+	}
+	
+	// Escapes a string with mysqli_real_escape_string
+	public static function escape($var, $connection = null) {
+		if(self::isConnection($connection)) {
+			return $connection->real_escape_string($var);
+		} elseif(self::isConnection(self::get())) {
+			return self::get()->real_escape_string($var);
+		}
+		// at least do something... (throw Exception)
+		return mysql_escape_string($var);
+	}
+	
+	// Shortcut for DBi::escape()
+	public static function e() {
+		return call_user_func_array('DBi::escape', func_get_args());
+	}
+	
+}
---
+++ Query.php
@@ -0,0 +1,240 @@
+<?php
+
+/**
+ * Query.php
+ * Wolfgang Drescher - wolfgangdrescher.ch
+ * This class allows you to send queries to a database with the PHP MySQLi class.
+ */
+
+require_once 'DBi.php';
+
+class Query {
+	
+	public static $throwExceptions = true;
+	
+	private $sql = '';
+	private $duration = 0;
+	private $connection = null;
+	private $result = null;
+	private $errno = null;
+	private $error = '';
+	
+	
+	public function __construct($sql, $connection = null) {
+		$this->setSql($sql);
+		$this->setConnection($connection);
+		$this->send();
+	}
+	
+	public function __destruct() {
+		if(!$this->isError()) {
+			$this->getResult()->close();
+		}
+	}
+	
+	public function __toString() {
+		ob_start();
+		echo "\n";
+		echo '<div class="panel panel-default">'."\n";
+		echo '	<div class="panel-heading"><h3 class="panel-title">PANEL HEADING WITH TITLE</h3></div>'."\n";
+		echo '	<div class="panel-body">'."\n";
+		echo '		<div><pre>'.htmlentities($this->getSql()).'</pre></div>'."\n";
+		if($this->isError()) {
+			// echo error
+		} else {
+			$cols = $this->getResult()->fetch_fields();
+			$tables = array();
+			foreach($cols as $value) {
+				if(!in_array($value->orgtable, $tables)) {
+					$tables[] = $value->orgtable;
+				}
+			}
+			echo '		<div>Affected rows: ' . $this->rows() . "</div>\n";
+			echo '		<div>Duration: ' . $this->getDuration(7) * 1000 . " milliseconds</div>\n";
+			echo '		<div>Affected tables: <code>'.implode('</code>, <code>', $tables)."</code></div>\n";
+		}
+		echo '	</div>'."\n";
+		if($this->rows() > 0) {
+			echo '	<div class="table-responsive" style="overflow-x: auto;">'."\n";
+			echo '		<table class="table table-striped table-bordered table-hover">'."\n";
+			echo '			<thead>'."\n";
+			echo '				<tr>'."\n";
+			echo '					<th>#</th>'."\n";
+			foreach($cols as $key => $value) {
+				echo '					<th>';
+				echo $value->name == $value->orgname ? (count($tables) > 1 ? $value->table . '.'.$value->orgname.''  : $value->orgname) : '`'.$value->name.'`';
+				echo '</th> '."\n";
+			}
+			echo '				</tr>'."\n";
+			echo '			</thead>'."\n";
+			echo '			<tbody>'."\n";
+			foreach($this->fetchAll('num') as $key => $row) {
+				echo '				<tr>'."\n";
+				echo '					<td>' .  intval($key + 1) . '</td>'."\n";
+				echo '					<td>'.implode($row, "</td>\n					<td>").'</td>'."\n";
+				echo '				</tr>'."\n";
+			}
+			echo '			</tbody>'."\n";
+			echo '		</table>'."\n";
+			echo '	</div>'."\n";
+		}
+		echo '</div>'."\n";
+		return ob_get_clean();
+	}
+	
+	public function getSql() {
+		return $this->sql;
+	}
+	
+	public function setSql($sql) {
+		$this->sql = trim($sql);
+	}
+	
+	public function getDuration($decimals = null) {
+		if($decimals === null) {
+			return $this->duration;
+		}
+		return number_format($this->duration, $decimals);
+	}
+	
+	public function setDuration($duration) {
+		$this->duration = $duration;
+	}
+	
+	public function getResult() {
+		return $this->result;
+	}
+	
+	public function setResult($result) {
+		$this->result = $result;
+	}
+	
+	public function getErrno() {
+		return $this->errno;
+	}
+	
+	public function setErrno($errno) {
+		$this->errno = intval($errno);
+	}
+	
+	public function getError() {
+		return $this->error;
+	}
+	
+	public function setError($error) {
+		$this->error = $error;
+	}
+	
+	public function getConnection() {
+		return $this->connection;
+	}
+	
+	public function setConnection($connection) {
+		if(DBi::isConnection($connection)) {
+			$this->connection = $connection;
+		} elseif((is_string($connection) OR is_int($connection)) AND DBi::isConnection(DBi::get($connection))) {
+			$this->connection = DBi::get($connection);
+		} elseif($connection === null AND DBi::isConnection(DBi::get())) {
+			$this->connection = DBi::get();
+		} else {
+			$this->connection = null;
+		}
+	}
+	
+	public function isError() {
+		return !$this->getResult() ? true : false;
+	}
+	
+	public function insertId() {
+		return $this->isError() ? null : intval($this->getConnection()->insert_id);
+	}
+	
+	public function rows() {
+		return $this->isError() ? null : $this->getResult()->num_rows;
+	}
+	
+ 	public function seek($rec = 0) {
+		return $this->isError() ? null : $this->getResult()->data_seek($rec);
+	}
+	
+	public function fetch($mode = 'assoc') {
+		if($mode == 'row') {
+			return $this->fetchRow();
+		} elseif($mode == 'object') {
+			return $this->fetchObject();
+		} elseif($mode == 'array') {
+			return $this->fetchArray();
+		} elseif($mode == 'all') {
+			return $this->fetchAll();
+		}
+		return $this->fetchAssoc();
+	}
+	
+	public function fetchAssoc() {
+		return $this->isError() ? false : $this->getResult()->fetch_assoc();
+	}
+	
+	public function fetchRow() {
+		return $this->isError() ? false : $this->getResult()->fetch_row();
+	}
+	
+	public function fetchArray($type = 'both') {
+		$mysqliConst = array('num' => MYSQLI_NUM, 'assoc' => MYSQLI_ASSOC, 'both' => MYSQLI_BOTH);
+		$type = in_array($type, array('num', 'assoc', 'both'))? $type : 'both';
+		return $this->isError() ? false : $this->getResult()->fetch_array($mysqliConst[$type]);
+	}
+	
+	public function fetchObject() {
+		return $this->isError() ? false : $this->getResult()->fetch_object();
+	}
+	
+	public function fetchAll($type = 'both') {
+		if($this->isError()) {
+			return false;
+		}
+		$data = array();
+		$this->seek(0);
+		while($row = $this->fetchArray($type)) {
+			$data[] = $row;
+		}
+		$this->seek(0);
+		return $data;
+	}
+	
+	function send($params = null) {
+		try {
+			if(!DBi::isConnection($this->getConnection())) {
+				throw new Exception('No MySQLi connection available.');
+			}
+			$timestart = microtime(true);
+			$result = $this->getConnection()->query($this->getSql());
+			$this->setDuration(microtime(true) - $timestart);
+			$this->setResult($result);
+			if($this->isError()) {
+				$this->setError($this->getConnection()->error);
+				$this->setErrno($this->getConnection()->errno);
+				throw new Exception($this->getError(), $this->getErrno());
+			}
+			
+		} catch(Exception $e) {
+			if(self::$throwExceptions === true) {
+				ob_start();
+				echo '<div class="alert alert-danger">';
+				echo $e->getMessage();
+				echo '<pre>'.htmlentities($this->getSql()).'</pre>';
+				echo '</div>';
+				echo ob_get_clean();
+			}
+		}
+	}
+	
+	public function returnJSON($quotes = false, $type = 'assoc') {
+		$json = json_encode($this->fetchAll($type));
+		return $quotes === true ? '\''.$json.'\'' : $json;
+	}
+	
+	public function echoJSON($quotes = false, $type = 'assoc') {
+		echo $this->returnJSON($quotes, $type);
+	}
+	
+}
